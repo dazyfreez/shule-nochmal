482d0c6775dc5f4ae4764ddd5c09430a539aaebe
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-08 19:30:11 +0200

    -

--- addieren.java
+++ addieren.java
@@ -1,3 +1,14 @@
 public class addieren {
+    public static int main(int n) {    
+        int erg = 0;
+        for (int i =1; i<=n; i++)
+        {
+            erg= erg+i;
+        }
+        return erg;
+    }
+        
     
-}
+}      
+    
+

81e2f7a37f510da4c6fdce820b4c4cbad71ece06
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-08 19:18:53 +0200

    -

---
+++ hochn.java
@@ -0,0 +1,11 @@
+public class hochn {
+    public static void main(String[] args) {
+        int n = 4;
+        int erg = 1;
+        for (int i = 1; i<=n; i++){
+            erg = erg*n;
+        }
+        System.out.println(erg);
+    }
+
+}

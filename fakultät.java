1e1beede7f63303d286e4ae38e699678622a8ba3
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-08 19:23:02 +0200

    -

--- fakultät.java
+++ fakultät.java
@@ -1,5 +1,10 @@
 public class fakultät {
     public static void main(String[] args){
-
+        int n = 4;
+        int erg = 1;
+        for (int i = 1; i<=n; i++) {
+            erg= erg *i;
+        }
+        
     }
 }
